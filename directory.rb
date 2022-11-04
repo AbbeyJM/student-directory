db6e284b0d2e7d210871517f97e709f863898116
Abbey <abbeyjeannieminett@gmail.com>
2022-11-04 18:47:11 +0000

    Final commit

--- directory.rb
+++ directory.rb
@@ -10,7 +10,7 @@ end
 def interactive_menu
     loop do
         print_menu
-        process(gets.chomp)
+        process(STDIN.gets.chomp)
     end
 end
 
@@ -35,13 +35,13 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # getting first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while name isn't empty repeat this code
   while !name.empty? do
     # adding student hash to array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   @students
 end
@@ -79,4 +79,26 @@ def save_students
     file.close
 end
 
+def load_students(filename = "students.csv")
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+    filename = ARGV.first # first argument from command line
+    return if filename.nil?
+    if File.exist?(filename)
+      load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+    else
+      puts "Sorry, #{filename} doesn't exist."
+      exit
+    end
+end
+
+try_load_students
 interactive_menu
